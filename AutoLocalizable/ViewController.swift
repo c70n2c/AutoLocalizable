47479ff60495d94839e6ed4e1d620af5a012cb86
c70n2c <c70n2c@gmail.com>
2020-05-06 17:19:09 +0800

    Initial Commit

---
+++ AutoLocalizable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLocalizable
+//
+//  Created by zowell on 2020/5/6.
+//  Copyright © 2020 Chancc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
