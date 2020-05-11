4bd52c8e7c71084169642b6fe7a8c06d7313f583
c70n2c <c70n2c@gmail.com>
2020-05-11 09:42:12 +0800

    final

--- AutoLocalizable/XlsxDecoder.swift
+++ AutoLocalizable/XlsxDecoder.swift
@@ -9,6 +9,18 @@
 import UIKit
 import CoreXLSX
 
+/*
+ 
+ 使用方法，请在电脑模拟器上跑 （README1、README2）
+
+ 1、将 1.xlsx 复制到桌面，这是一个对照表做了如下操作
+    * 清除表格多余的行与列(没用到的行与列)
+    * 全选（cmd + a），清除表格所有内容的格式
+    * 只能有一张（Sheet1）表格
+ 2、设置输入文件路径 let sourcePath = "/Users/xxx/Desktop/1.xlsx"
+ 3、设置输出路径 let wirtePath = "/Users/xxx/Desktop/"
+ */
+
 /// 输入文件路径
 let sourcePath = "/Users/zowell/Desktop/1.xlsx"
 /// 输出路径
